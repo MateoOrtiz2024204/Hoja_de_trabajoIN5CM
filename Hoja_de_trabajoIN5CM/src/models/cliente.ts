0928a2c09e85d79fc2e21ca69b24041f54bb86de
Mateo <mortiz-2024204@kinal.edu.gt>
2026-07-21 08:36:52 -0600

    Feat: Se agrego el modelo de cliente

---
+++ Hoja_de_trabajoIN5CM/src/models/cliente.ts
@@ -0,0 +1,11 @@
+export interface Cliente {
+
+    id: number;
+    nombre: string;
+    apellido: string;
+    correo: string;
+    telefono: string;
+    direccion: string;
+    estado: string;
+
+}
